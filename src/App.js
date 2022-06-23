2568b3b962dcb429cba966292e6a852a264833b9
Omkar Sawant <omkar18@somaiya.edu>
2022-06-23 18:39:26 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+
 import {sha256} from 'crypto-hash';
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
+import { useEffect } from 'react';
 
 function App() {
 
-  const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
-      .then(FingerprintJS => FingerprintJS.load())
 
     const useragent = navigator.userAgent
 
@@ -19,28 +20,48 @@ function App() {
   
   
   
-    fpPromise
-      .then(fp => fp.get())
-      .then(result => {
-        // This is the visitor identifier:
-        const visitorId = result.visitorId
-        console.log(result)
-        mainObj["timeZone"] = result.components.timezone.value
-        mainObj["userAgent"] = useragent
-        mainObj["canvas"] = result.components.canvas.value
-        mainObj["screenRes"] = result.components.screenResolution.value
-        mainObj["plugins"] = result.components.plugins.value
-        mainObj["fontsJS"] = result.components.fonts.value
-        mainObj["deviceMemory"] = result.components.deviceMemory.value
-        // console.log(result.components.sessionStorage)
-        // console.log(mainObj)
-        // console.log(JSON.stringify(mainObj))
-        hash(JSON.stringify(mainObj))
-      })
+    // fpPromise
+    //   .then(fp => fp.get())
+    //   .then(result => {
+    //     // This is the visitor identifier:
+    //     const visitorId = result.visitorId
+    //     console.log(result)
+    //     mainObj["timeZone"] = result.components.timezone.value
+    //     mainObj["userAgent"] = useragent
+    //     mainObj["canvas"] = result.components.canvas.value
+    //     mainObj["screenRes"] = result.components.screenResolution.value
+    //     mainObj["plugins"] = result.components.plugins.value
+    //     mainObj["fontsJS"] = result.components.fonts.value
+    //     mainObj["deviceMemory"] = result.components.deviceMemory.value
+    //     // console.log(result.components.sessionStorage)
+    //     // console.log(mainObj)
+    //     // console.log(JSON.stringify(mainObj))
+    //     hash(JSON.stringify(mainObj))
+    //   })
+
+    
+      useEffect(() => {
+        FingerprintJS.load()
+            .then((fp) => fp.get())
+            .then((result) => {
+              console.log(result)
+              mainObj["timeZone"] = result.components.timezone.value
+              mainObj["userAgent"] = useragent
+              mainObj["canvas"] = result.components.canvas.value
+              mainObj["screenRes"] = result.components.screenResolution.value
+              mainObj["plugins"] = result.components.plugins.value
+              mainObj["fontsJS"] = result.components.fonts.value
+              mainObj["deviceMemory"] = result.components.deviceMemory.value
+              // console.log(result.components.sessionStorage)
+              // console.log(mainObj)
+              // console.log(JSON.stringify(mainObj))
+              hash(JSON.stringify(mainObj))
+            });
+    }, []);
 
   return (
     <div className="App">
-      TEst
+      TEst hello
     </div>
   );
 }
